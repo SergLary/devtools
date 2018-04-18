3099d949384aa634f54550d9b692ad46e64db51b
Serg Lar <mylatunys@gmail.com>
2018-04-18 16:23:37 +0300

    decompose to 4 main components

---
+++ client/imports/Sidebar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+	render() {
+		return (
+      <nav className="side-navbar">
+        {/* Side navbar */}
+        <div className="side-navbar-wrapper">
+          {/* Sidebar Header    */}
+          <div className="sidenav-header d-flex align-items-center justify-content-center">
+            {/* User Info*/}
+            <div className="sidenav-header-inner text-center"><img src="img/avatar-1.jpg" alt="person" className="img-fluid rounded-circle" />
+              <h2 className="h5">Anderson Hardy</h2><span>Web Developer</span>
+            </div>
+            {/* Small Brand information, appears on minimized sidebar*/}
+            <div className="sidenav-header-logo"><a href="index.html" className="brand-small text-center"> <strong>B</strong><strong className="text-primary">D</strong></a></div>
+          </div>
+          {/* Sidebar Navigation Menus*/}
+          <div className="main-menu">
+            <h5 className="sidenav-heading">Main</h5>
+            <ul id="side-main-menu" className="side-menu list-unstyled">                  
+              <li><a href="index.html"> <i className="icon-home" />Home                           </a></li>
+              <li><a href="forms.html"> <i className="icon-form" />Forms                           </a></li>
+              <li><a href="charts.html"> <i className="fa fa-bar-chart" />Charts                           </a></li>
+              <li><a href="tables.html"> <i className="icon-grid" />Tables                           </a></li>
+              <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i className="icon-interface-windows" />Example dropdown </a>
+                <ul id="exampledropdownDropdown" className="collapse list-unstyled ">
+                  <li><a href="#">Page</a></li>
+                  <li><a href="#">Page</a></li>
+                  <li><a href="#">Page</a></li>
+                </ul>
+              </li>
+              <li><a href="login.html"> <i className="icon-interface-windows" />Login page                           </a></li>
+              <li> <a href="#"> <i className="icon-mail" />Demo
+                  <div className="badge badge-warning">6 New</div></a></li>
+            </ul>
+          </div>
+          <div className="admin-menu">
+            <h5 className="sidenav-heading">Second menu</h5>
+            <ul id="side-admin-menu" className="side-menu list-unstyled"> 
+              <li> <a href="#"> <i className="icon-screen"> </i>Demo</a></li>
+              <li> <a href="#"> <i className="icon-flask"> </i>Demo
+                  <div className="badge badge-info">Special</div></a></li>
+              <li> <a href="#"> <i className="icon-flask"> </i>Demo</a></li>
+              <li> <a href="#"> <i className="icon-picture"> </i>Demo</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+		)
+	}
+}	
